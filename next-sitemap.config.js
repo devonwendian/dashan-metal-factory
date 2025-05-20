e0fd3c6911b53d1b2f495695da1a6d43fb8f878f
devonwen <devonwen@tencent.com>
2025-05-21 07:59:50 +0800

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@ module.exports = {
   generateRobotsTxt: true, // 自动生成 robots.txt
   sitemapSize: 7000, // 每个 sitemap 文件的最大 URL 数
   changefreq: 'weekly', // 更新频率，可选：always, hourly, daily, weekly, monthly, yearly, never
-  priority: 0.7, // 默认优先级
+  priority: 1, // 默认优先级
   i18n: {
     defaultLocale: 'en', // 默认语言
     locales: ['en', 'zh'], // 支持的语言
