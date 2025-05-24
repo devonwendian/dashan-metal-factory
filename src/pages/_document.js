0459ee79eeb91a0adf8f200c303f4e9c58460d95
devonwen <devonwen@tencent.com>
2025-05-24 16:28:33 +0800

    seo 优化

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,9 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="alternate" hrefLang="zh" href="https://yourwebsite.com/zh" />
-        <link rel="alternate" hrefLang="en" href="https://yourwebsite.com/en" />
-        <link rel="alternate" hrefLang="x-default" href="https://yourwebsite.com" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body class="bg-white font-sans antialiased">
         <Main />
