82ce7c9e41204c95cf9726e655c299ed853cb07c
devonwen <devonwen@tencent.com>
2025-05-26 23:30:55 +0800

    添加谷歌分析

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ export default function Document() {
       <body class="bg-white font-sans antialiased">
         <Main />
         <NextScript />
-        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-2R1R88K0V" />
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-G2R1R88K0V" />
         <Script
           id="google-analytics"
           strategy="afterInteractive"
@@ -19,7 +19,7 @@ export default function Document() {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-2R1R88K0V');
+              gtag('config', 'G-G2R1R88K0V');
             `,
           }}
         />
