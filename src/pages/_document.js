ffb7bf796210cb73cdda0f824ef10e5694e0d847
devonwen <devonwen@tencent.com>
2025-05-26 23:28:48 +0800

    添加谷歌分析

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -9,6 +10,19 @@ export default function Document() {
       <body class="bg-white font-sans antialiased">
         <Main />
         <NextScript />
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-2R1R88K0V" />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-2R1R88K0V');
+            `,
+          }}
+        />
       </body>
     </Html>
   );
